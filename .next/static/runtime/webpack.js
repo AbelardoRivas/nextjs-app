9c6adb2643b941473bf15bcebc1f506d0a1773c2
Abelardo Rivas <abelardo.rivas1@gmail.com>
2019-02-15 09:48:56 -0400

    Estilos CSS en Next.js

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "de2507790f6c031c6ff2";
+/******/ 	var hotCurrentHash = "6b969b2372fd5b564e0a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
